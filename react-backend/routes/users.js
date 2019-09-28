bbcb4b919d8cb9137de98b0873df36381aa14663
Tharun <tharunsnest@gmail.com>
2019-09-27 21:22:48 -0700

    Research task basic application

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -10,11 +10,11 @@ router.get("/", function(req, res, next) {
   res.json([
     {
       id: 1,
-      username: "Aravinda Sai"
+      username: "Tharun"
     },
     {
       id: 2,
-      username: "Maverick"
+      username: "Chintham"
     }
   ]);
 });
