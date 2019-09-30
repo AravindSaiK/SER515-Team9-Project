34f4977633f0bddcbc30f3b3012e721f5f09204d
Tharun <tharunsnest@gmail.com>
2019-09-29 19:14:15 -0700

    adding number block panel

--- interactivemathlearning/src/App.js
+++ interactivemathlearning/src/App.js
@@ -8,6 +8,9 @@ class App extends Component{
       <div>
           <div className="row">
           <h1>Interactive Math Learning</h1>
+          <div className="NumberPanel">
+          <span className="task-header">Number Panel</span>
+          </div>
           <div className="ResultPanel">
           <span className="task-header">Result Panel</span> 
           </div>
