9d66bfb73c335e71dffa8aae5f44abab07e56a22
venkata sai ram <veadala@asu.edu>
2019-10-02 21:44:29 -0700

    Implemented US55-Task57

--- interactivemathlearning/src/App.js
+++ interactivemathlearning/src/App.js
@@ -1,89 +1,77 @@
 import React , { Component } from 'react';
 import "./App.css"
 
-
 class App extends Component{
-	state = {
-	        tasks: [
-	            {name:"1",category:"NumberPanel", bgcolor: "SkyBlue"},
-	            {name:"2", category:"NumberPanel", bgcolor:"SkyBlue"},
-	            {name:"3", category:"NumberPanel", bgcolor:"SkyBlue"},
-	            {name:"4",category:"NumberPanel", bgcolor: "SkyBlue"},
-	            {name:"5", category:"NumberPanel", bgcolor:"SkyBlue"},
-	            {name:"6", category:"NumberPanel", bgcolor:"SkyBlue"},
-	            {name:"7",category:"NumberPanel", bgcolor: "SkyBlue"},
-	            {name:"8", category:"NumberPanel", bgcolor:"SkyBlue"},
-	            {name:"9", category:"NumberPanel", bgcolor:"SkyBlue"},
-	            {name:"0", category:"NumberPanel", bgcolor:"SkyBlue"}
-	          ]
-	    }
-	onDragStart = (ev, name) => {
-		console.log('dragstart:',name);
-		ev.dataTransfer.setData("name", name);
-	}
+    state = {
+        tasks: [
+            {name:"1",category:"NumberPanel", bgcolor: "SkyBlue"},
+            {name:"2", category:"NumberPanel", bgcolor:"SkyBlue"},
+            {name:"3", category:"NumberPanel", bgcolor:"SkyBlue"},
+            {name:"4",category:"NumberPanel", bgcolor: "SkyBlue"},
+            {name:"5", category:"NumberPanel", bgcolor:"SkyBlue"},
+            {name:"6", category:"NumberPanel", bgcolor:"SkyBlue"},
+            {name:"7",category:"NumberPanel", bgcolor: "SkyBlue"},
+            {name:"8", category:"NumberPanel", bgcolor:"SkyBlue"},
+            {name:"9", category:"NumberPanel", bgcolor:"SkyBlue"},
+            {name:"0", category:"NumberPanel", bgcolor:"SkyBlue"},
+			{name:"+", category:"NumberPanel", bgcolor:"SkyBlue"},
+			{name:"-", category:"NumberPanel", bgcolor:"SkyBlue"},
+			{name:"*", category:"NumberPanel", bgcolor:"SkyBlue"},
+			{name:"/", category:"NumberPanel", bgcolor:"SkyBlue"}
+          ],NumberPanel: [],
+            SandBoxPanel: []
+    }
 
-	onDragOver = (ev) => {
-		ev.preventDefault();
-	}
+    onDragStart = (ev, name) => {
+        ev.dataTransfer.dropEffect = "move";
+        ev.dataTransfer.setData("name", name);
+    }
 
-	onDrop = (ev, cat) => {
-		let id = ev.dataTransfer.getData("name");
+    onDragOver = (ev) => {
+        ev.preventDefault();
+    }
 
-		let tasks = this.state.tasks.filter((task) => {
-			if (task.name === id) {
-				task.category = cat;
-			}
-			return task;
-		});
-
-		this.setState({
-			...this.state,
-			tasks
-		});
-	}
-
-	render() {
-		var tasks = {
-		           NumberPanel: [],
-		           SandBoxPanel: [],
-		           ResultPanel: []
-		       }
-		       this.state.tasks.forEach ((t) => {
-		           tasks[t.category].push(
-		               <div key={t.name}
-							onDragStart = {(e) => this.onDragStart(e, t.name)}
-		                   draggable
-		                   className="draggable"
-		                   style = {{backgroundColor: t.bgcolor,width:"25px",textAlign:"center",padding:"10px",margin:"5px",borderRadius:"6px"}}
-		               >
-		                   {t.name}
-		               </div>
-		           );
-		       });
-		return (
+    onDropLeft = e =>{
+        e.preventDefault();
+        const data = e.dataTransfer.getData("name");
+        let {SandBoxPanel} = this.state;
+        SandBoxPanel.push(data);
+        this.setState({ SandBoxPanel });
+    }
+	
+	
+    
+    render() {
+	const { tasks,NumberPanel,SandBoxPanel }=this.state;
+        return (
       <div>
           <div className="row">
-          <h1>Interactive Math Learning</h1>
-          <div className="NumberPanel"
-			onDragOver={(e)=>this.onDragOver(e)}
-			onDrop={(e)=>{this.onDrop(e, "NumberPanel")}}>
-			<span className="task-header">Number Panel</span>
-          {tasks.NumberPanel}
-          </div>
-           <div className="SandBoxPanel"
-			onDragOver={(e)=>this.onDragOver(e)}
-			onDrop={(e)=>this.onDrop(e, "SandBoxPanel")}>
-			<span className="task-header">SandBox Panel</span>
-			{tasks.SandBoxPanel}
-        </div>
+          <h1>Interactive Math Learning</h1> 
+          <div className="NumberPanel"> 
+		  <span className="task-header">Number Panel</span>    
+            {
+                        tasks.map((task) =>{
+                          return <p style = {{backgroundColor: "SkyBlue",width:"25px",textAlign:"center",padding:"10px",margin:"5px",borderRadius:"6px"}}
+						  draggable="true" onDragStart={ (ev) => this.onDragStart(ev, task.name) }>{task.name}</p>
+                        })
+            }		  
+             </div>
+
+          <div className="SandBoxPanel" onDragOver={this.onDragOver} onDrop={this.onDropLeft}> 
+		  <span className="task-header">SandBox Panel</span>
+                 {
+                    SandBoxPanel.map( itm =>{
+                      return <p>{itm}</p>
+                    })
+                  }		  
+                                   
+          {tasks.SandBoxPanel}</div>
           <div className="ResultPanel">
-          <span className="task-header">Result Panel</span> 
+          <span className="task-header">Result Panel</span>
           </div>
           </div>
-
-
           </div>
-);           
+        );           
     }
 
 }
