390652bbde475427b1617b4019e3f1d5111d2499
Aravind <akondama@asu.edu>
2019-11-02 23:51:58 -0700

    Linked Login functionality

--- interactivemathlearning/src/Main.js
+++ interactivemathlearning/src/Main.js
@@ -1,40 +1,35 @@
-import React, {Component} from 'react';
-import {BrowserRouter,
-		Switch,
-		Link,
-		Route} from "react-router-dom";
-import FirstGradeApp from './FirstGrade';
-import ThirdGradeApp from './ThirdGrade';
-import NavBar from './NavBar';
-import FirstGrade from './FirstGrade';
-import ThirdGrade from './ThirdGrade';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Link, Route } from "react-router-dom";
+import FirstGradeApp from "./FirstGrade";
+import ThirdGradeApp from "./ThirdGrade";
+import NavBar from "./NavBar";
+import FirstGrade from "./FirstGrade";
+import ThirdGrade from "./ThirdGrade";
+import Login from "./Login";
 
-class Main extends Component{
-	render(){
-		return(
-			<BrowserRouter>
-			<div className="Main">
-			<Switch>
-		<Route exact path='/Main' component={NavBar} />
-		<Route exact path='/FirstGrade' component={FirstGrade} />
-		<Route exact path='/ThirdGrade' component={ThirdGrade} />
-
-
-		</Switch>
-		</div>
-		</BrowserRouter>
-		//<Login/>
-	)
-	}
-
+class Main extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="Main">
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/Main" component={NavBar} />
+            <Route exact path="/FirstGrade" component={FirstGrade} />
+            <Route exact path="/ThirdGrade" component={ThirdGrade} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
-function Home(){
-	return (
-		<div>
-		<NavBar/>
-		</div>
-);
+function Home() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
 
 export default Main;
