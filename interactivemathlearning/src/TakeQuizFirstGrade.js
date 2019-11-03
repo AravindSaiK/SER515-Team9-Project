f21be6583752f4f971a07061a5845de9b875281b
Aravind <akondama@asu.edu>
2019-11-03 16:37:47 -0700

    Added first grade quiz

---
+++ interactivemathlearning/src/TakeQuizFirstGrade.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Link } from "react-router-dom";
+
+export default function TakeQuizFirstGrade() {
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Interactive Math Learning
+          </Typography>
+          <Button color="inherit">Logout</Button>
+        </Toolbar>
+      </AppBar>
+      <div align="center" style={{ marginTop: "275px" }}>
+        <Button
+          variant="contained"
+          color="default"
+          className={classes.buttonClass}
+        >
+          <Link to="/EasyFirst">Easy</Link>
+        </Button>
+        <Button
+          variant="contained"
+          color="default"
+          className={classes.buttonClass}
+        >
+          <Link to="/MediumFirst">Medium</Link>
+        </Button>
+        <Button
+          variant="contained"
+          color="default"
+          className={classes.buttonClass}
+        >
+          <Link to="/AdvancedThird">Advanced</Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
