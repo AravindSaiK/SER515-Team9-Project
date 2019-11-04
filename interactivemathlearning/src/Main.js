efef2a9e3b3a14a90ee0e55f6d1b2e34867b92a1
Tharun <tharunsnest@gmail.com>
2019-11-03 19:16:45 -0700

    Adding quiz routes

--- interactivemathlearning/src/Main.js
+++ interactivemathlearning/src/Main.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import FirstGrade from "./FirstGrade";
 import ThirdGrade from "./ThirdGrade";
 import NavBar from "./NavBar";
@@ -8,7 +8,7 @@ import FirstGradeMenu from "./FirstGradeMenu";
 import TakeQuizFirstGrade from "./TakeQuizFirstGrade";
 import ThirdGradeMenu from "./ThirdGradeMenu";
 import TakeQuizThirdGrade from "./TakeQuizThirdGrade";
-
+import QuestionsEasy from "./QuestionsEasy";
 /**
  * @author Nikhila Saini ,Venkata Sairam
  * Since Nov 3,2019
@@ -20,17 +20,23 @@ class Main extends Component {
             <BrowserRouter>
                 <div className="Main">
                     <Switch>
-                        <Route exact path="/" component={Login}/>
-						<Route exact path='/logout' component={Login} />
-                        <Route exact path="/Main" component={NavBar}/>
-                        <Route exact path='/FirstGradeMenu' component={FirstGradeMenu}/>
-						<Route exact path='/ThirdGradeMenu' component={ThirdGradeMenu}/>
-						<Route exact path='/TakeQuizFirstGrade' component={TakeQuizFirstGrade} />
-		                <Route exact path='/TakeQuizThirdGrade' component={TakeQuizThirdGrade} />
-                        <Route exact path="/FirstGrade" component={FirstGrade}/>
-                        <Route exact path="/ThirdGrade" component={ThirdGrade}/>
-						<Route exact path='/PracticeFirstGrade' component={FirstGrade} />
-						<Route exact path='/PracticeThirdGrade' component={ThirdGrade} />
+                        <Route exact path="/" component={Login} />
+                        <Route exact path='/logout' component={Login} />
+                        <Route exact path="/Main" component={NavBar} />
+                        <Route exact path='/FirstGradeMenu' component={FirstGradeMenu} />
+                        <Route exact path='/ThirdGradeMenu' component={ThirdGradeMenu} />
+                        <Route exact path='/TakeQuizFirstGrade' component={TakeQuizFirstGrade} />
+                        <Route exact path='/TakeQuizThirdGrade' component={TakeQuizThirdGrade} />
+                        <Route exact path="/FirstGrade" component={FirstGrade} />
+                        <Route exact path="/ThirdGrade" component={ThirdGrade} />
+                        <Route exact path='/PracticeFirstGrade' component={FirstGrade} />
+                        <Route exact path='/PracticeThirdGrade' component={ThirdGrade} />
+                        <Route exact path='/EasyFirst' component={QuestionsEasy} />
+                        <Route exact path='/MediumFirst' component={QuestionsEasy} />
+                        <Route exact path='/AdvancedFirst' component={QuestionsEasy} />
+                        <Route exact path='/EasyThird' component={QuestionsEasy} />
+                        <Route exact path='/MediumThird' component={QuestionsEasy} />
+                        <Route exact path='/AdvancedThird' component={QuestionsEasy} />
                     </Switch>
                 </div>
             </BrowserRouter>
@@ -41,7 +47,7 @@ class Main extends Component {
 function Home() {
     return (
         <div>
-            <NavBar/>
+            <NavBar />
         </div>
     );
 }
