13c5aa032ceaea27508c56693a4823a32ecb3fae
venkata sai ram <veadala@asu.edu>
2019-11-03 19:00:24 -0700

    Done US28-TASK89

--- interactivemathlearning/src/Main.js
+++ interactivemathlearning/src/Main.js
@@ -4,8 +4,10 @@ import FirstGrade from "./FirstGrade";
 import ThirdGrade from "./ThirdGrade";
 import NavBar from "./NavBar";
 import Login from "./Login";
-import FirstGradeMenu from './FirstGradeMenu';
-import ThirdGradeMenu from './ThirdGradeMenu';
+import FirstGradeMenu from "./FirstGradeMenu";
+import TakeQuizFirstGrade from "./TakeQuizFirstGrade";
+import ThirdGradeMenu from "./ThirdGradeMenu";
+import TakeQuizThirdGrade from "./TakeQuizThirdGrade";
 
 /**
  * @author Nikhila Saini ,Venkata Sairam
@@ -23,6 +25,8 @@ class Main extends Component {
                         <Route exact path="/Main" component={NavBar}/>
                         <Route exact path='/FirstGradeMenu' component={FirstGradeMenu}/>
 						<Route exact path='/ThirdGradeMenu' component={ThirdGradeMenu}/>
+						<Route exact path='/TakeQuizFirstGrade' component={TakeQuizFirstGrade} />
+		                <Route exact path='/TakeQuizThirdGrade' component={TakeQuizThirdGrade} />
                         <Route exact path="/FirstGrade" component={FirstGrade}/>
                         <Route exact path="/ThirdGrade" component={ThirdGrade}/>
 						<Route exact path='/PracticeFirstGrade' component={FirstGrade} />
