2332f1e69e945e60b96bbb2767ff3607897a0dbf
venkata sai ram <veadala@asu.edu>
2019-11-03 18:52:25 -0700

    Implemented Logout Functionality

--- interactivemathlearning/src/Main.js
+++ interactivemathlearning/src/Main.js
@@ -1,56 +1,45 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, {Component} from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import FirstGrade from "./FirstGrade";
 import ThirdGrade from "./ThirdGrade";
 import NavBar from "./NavBar";
 import Login from "./Login";
-import FirstGradeMenu from "./FirstGradeMenu";
-import TakeQuizFirstGrade from "./TakeQuizFirstGrade";
-import ThirdGradeMenu from "./ThirdGradeMenu";
-import TakeQuizThirdGrade from "./TakeQuizThirdGrade";
+import FirstGradeMenu from './FirstGradeMenu';
+import ThirdGradeMenu from './ThirdGradeMenu';
 
 /**
- * @author Nikhila Saini ,Venkata Sairam, Tharun Chintham, Aravinda Sai Kondamari
+ * @author Nikhila Saini ,Venkata Sairam
  * Since Nov 3,2019
  */
 
 class Main extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="Main">
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/Main" component={NavBar} />
-            <Route exact path="/FirstGradeMenu" component={FirstGradeMenu} />
-            <Route exact path="/ThirdGradeMenu" component={ThirdGradeMenu} />
-            <Route exact path="/FirstGrade" component={FirstGrade} />
-            <Route exact path="/ThirdGrade" component={ThirdGrade} />
-            <Route exact path="/PracticeFirstGrade" component={FirstGrade} />
-            <Route
-              exact
-              path="/TakeQuizFirstGrade"
-              component={TakeQuizFirstGrade}
-            />
-            <Route exact path="/PracticeThirdGrade" component={ThirdGrade} />
-            <Route
-              exact
-              path="/TakeQuizThirdGrade"
-              component={TakeQuizThirdGrade}
-            />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="Main">
+                    <Switch>
+                        <Route exact path="/" component={Login}/>
+						<Route exact path='/logout' component={Login} />
+                        <Route exact path="/Main" component={NavBar}/>
+                        <Route exact path='/FirstGradeMenu' component={FirstGradeMenu}/>
+						<Route exact path='/ThirdGradeMenu' component={ThirdGradeMenu}/>
+                        <Route exact path="/FirstGrade" component={FirstGrade}/>
+                        <Route exact path="/ThirdGrade" component={ThirdGrade}/>
+						<Route exact path='/PracticeFirstGrade' component={FirstGrade} />
+						<Route exact path='/PracticeThirdGrade' component={ThirdGrade} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 function Home() {
-  return (
-    <div>
-      <NavBar />
-    </div>
-  );
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    );
 }
 
 export default Main;
