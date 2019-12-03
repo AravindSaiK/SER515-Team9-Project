9b4856e95ab0977aecea02cbbe86e71cf6a310be
Nikhila6 <saini.nikhila@gmail.com>
2019-12-02 21:36:17 -0700

    Create and Manage quizzes by admin

--- interactivemathlearning/src/server/server.js
+++ interactivemathlearning/src/server/server.js
@@ -106,3 +106,128 @@ app.delete('/deleteStudent', function(req,res){
 
 })
 
+/**
+ All Quiz Handled Functions
+ */
+
+
+
+app.post('/createQuiz', function(req, res) {
+
+
+    let rawdata = fs.readFileSync('quizData.json');
+    let quiz = JSON.parse(rawdata);
+    /*console.log(student)
+    */
+    for(var i=0; i<quiz.length;i++) {
+        if(quiz[i].quiz.quizName == req.body.quizName){
+            res.status(500).json({"message": "Quiz already exists"})
+            return
+        }
+    }
+    quizzes = quiz
+    let newQuiz =  {
+        quiz: req.body
+    }
+    quizzes.push(newQuiz)
+    fs.writeFileSync('quizData.json', JSON.stringify(quizzes));
+
+    res.status(200).json({"message": "Quiz Added successfully"})
+});
+
+
+app.get('/viewQuizzes', function(req, res) {
+    let rawdata = fs.readFileSync('quizData.json');
+    let quizzes = JSON.parse(rawdata);
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+    });
+    res.end(JSON.stringify(quizzes));
+});
+
+
+app.delete('/deleteQuiz', function(req,res){
+    let rawdata = fs.readFileSync('quizData.json');
+    let quizzes = JSON.parse(rawdata);
+    /*console.log(req.body.id)
+    */
+    for(var i=0; i<quizzes.length;i++) {
+        if(quizzes[i].quiz.quizName == req.body.id){
+            quizzes.splice(i,1)
+        }
+
+    }
+    fs.writeFileSync('quizData.json', JSON.stringify(quizzes),{encoding:'utf8',flag:'w'});
+    rawdata = fs.readFileSync('quizData.json');
+    quizzes = JSON.parse(rawdata);
+    res.status(200).json({"data": quizzes})
+
+})
+
+app.get('/viewQuizzesFirstGrade', function(req, res) {
+    let quizzes = []
+    let rawdata = fs.readFileSync('quizData.json');
+    let quizData = JSON.parse(rawdata);
+
+    for(var i=0; i<quizData.length;i++) {
+        if(quizData[i].quiz.grade == "First Grade"){
+            /*res.status(500).json({"message": "Quiz already exists"})
+         return*/
+
+            quizzes.push(quizData[i])
+        }
+    }
+
+
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+    });
+    res.end(JSON.stringify(quizzes));
+});
+
+
+app.get('/viewQuizzesThirdGrade', function(req, res) {
+    let quizzes = []
+    let rawdata = fs.readFileSync('quizData.json');
+    let quizData = JSON.parse(rawdata);
+
+    for(var i=0; i<quizData.length;i++) {
+        if(quizData[i].quiz.grade == "Third Grade"){
+            /*res.status(500).json({"message": "Quiz already exists"})
+         return*/
+
+            quizzes.push(quizData[i])
+        }
+    }
+
+
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+    });
+    res.end(JSON.stringify(quizzes));
+});
+
+app.get('/takeQuiz', function(req, res) {
+
+
+
+    let rawdata = fs.readFileSync('quizData.json');
+    let quiz = JSON.parse(rawdata);
+    /*console.log(student)
+    */
+    for(var i=0; i<quiz.length;i++) {
+        if(quiz[i].quiz.quizName == req.query.id){
+            res.status(200).json({"data": quiz[i]})
+            return
+        }
+    }
+    //console.log(req.query.id)
+    res.status(500).json({"data": "Quiz Doesn't Exist"})
+
+    /*let rawdata = fs.readFileSync('quizData.json');
+    let quizzes = JSON.parse(rawdata);
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+      });
+      res.end(JSON.stringify(quizzes));  */
+});
